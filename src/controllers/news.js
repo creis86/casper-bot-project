6ecda7bc8e8e19b25a6128648dffcec3747d3f7a
creis86 <creis.git@gmail.com>
2021-08-08 15:21:17 -0300

    Added news controller and refactored news route.

---
+++ src/controllers/news.js
@@ -0,0 +1,26 @@
+module.exports.render = (app, req, res) => {
+    app.config.db_connection();
+    const News = require("../models/news");
+
+    News.find().sort({updatedAt: -1})
+        .then((result) => {
+            res.render("news", {newsArray: result});
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+module.exports.create = (app, req, res) => {
+    app.config.db_connection();
+    const News = require("../models/news");
+    const news = new News(req.body);
+
+    news.save()
+        .then(() => {
+            res.redirect("/news");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,28 +1,9 @@
 module.exports = (app) => {
     app.get("/news", (req, res) => {
-        app.config.db_connection();
-        const News = require("../models/news");
-
-        News.find().sort({updatedAt: -1})
-            .then((result) => {
-                res.render("news", {newsArray: result});
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        app.src.controllers.news.render(app, req, res);
     });
 
     app.post("/news", (req, res) => {
-        app.config.db_connection();
-        const News = require("../models/news");
-        const news = new News(req.body);
-
-        news.save()
-            .then(() => {
-                res.redirect("/news");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        app.src.controllers.news.create(app, req, res);
     });
 };
